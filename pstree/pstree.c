d9184fdb44a688b68f3e9ff93de0a57b96c7f83d
tracer-nju <tracer@nju.edu.cn>
2022-12-07 03:57:47 +0000

    Linux 6e55bd16cc0c 5.15.49-linuxkit #1 SMP Tue Sep 13 07:51:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:57:47 up 16:34,  0 users,  load average: 0.06, 0.07, 0.01

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <assert.h>
+#define MAX_BUF 1024
 
 int main(int argc, char *argv[]) {
-  
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+  int process_id;
+
+  // for (int i = 0; i < argc; i++) {
+  //   assert(argv[i]);
+  //   printf("argv[%d] = %s\n", i, argv[i]);
+  // }
+  // assert(!argv[argc]);
+  FILE *stream = fopen("/proc", "r");
+  while ((fscanf(stream, "%s", &process_id) > 0)) {
+    printf("%d\n", process_id);
   }
-  assert(!argv[argc]);
   return 0;
 }
