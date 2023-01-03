3ebcc9357641d2f58e9ef080eb610e6804c7481a
tracer-nju <tracer@nju.edu.cn>
2023-01-03 13:25:35 +0000

    Linux 4f42aa569bc8 5.15.49-linuxkit #1 SMP Tue Sep 13 07:51:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:25:34 up 10 days, 12:23,  0 users,  load average: 0.34, 0.20, 0.08

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -138,7 +138,8 @@ static void test_2()
 
 int main()
 {
-    setbuf(stdout, NULL);
+    setvbuf(stdout, NULL,
+            _IONBF, BUFSIZ);
 
     printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
     test_1();
