413747fd6d513a13098aaba42639909a546fe09b
tracer-nju <tracer@nju.edu.cn>
2023-01-03 04:14:58 +0000

    Linux 4f42aa569bc8 5.15.49-linuxkit #1 SMP Tue Sep 13 07:51:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:14:58 up 10 days,  3:12,  0 users,  load average: 0.18, 0.13, 0.10

--- libco/co.c
+++ libco/co.c
@@ -129,7 +129,7 @@ static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg)
 {
   asm volatile(
 #if __x86_64__
-      "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+      "movq %0, %%rsp; movq %2, %%rdi; call *%1"
       :
       : "b"((uintptr_t)sp), "d"(entry), "a"(arg)
       : "memory"
