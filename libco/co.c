83ebe652435fba7024a6d3a198d6388c251d7ccf
tracer-nju <tracer@nju.edu.cn>
2023-01-03 02:37:24 +0000

    Linux 4f42aa569bc8 5.15.49-linuxkit #1 SMP Tue Sep 13 07:51:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:37:24 up 10 days,  1:35,  0 users,  load average: 0.06, 0.03, 0.36

--- libco/co.c
+++ libco/co.c
@@ -106,7 +106,7 @@ void co_yield ()
     if (current->status == CO_NEW)
     {
       current->status = CO_RUNNING;
-      stack_switch_call(current->stack, current->func, current->arg);
+      stack_switch_call(current->stack, current->func, (uintptr_t)current->arg);
     }
     else
     {
