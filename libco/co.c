b78a161d6ff1b2f356c7fbf16c0554d859faf8b1
tracer-nju <tracer@nju.edu.cn>
2023-01-06 07:21:35 +0000

    Linux 4f42aa569bc8 5.15.49-linuxkit #1 SMP Tue Sep 13 07:51:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:21:35 up 12 days, 17:11,  0 users,  load average: 0.05, 0.05, 0.01

--- libco/co.c
+++ libco/co.c
@@ -136,7 +136,7 @@ static inline void stack_switch_call(CO *co)
       "movq %%rsp, %0; movq %1, %%rsp; movq %3, %%rdi; call *%2; movq %0, %%rsp"
       : "+r"(co->rsp_backup)
       : "b"((uintptr_t)(co->stack + STACK_SIZE - 16)), "d"(co->func), "a"(co->arg)
-      : "memory", "%%rcx");
+      : "memory", "%rcx");
 #else
       "" ::
           : "memory");
