fdbb33d0c57a5ad63c67b6771b36f598bd273f67
tracer-nju <tracer@nju.edu.cn>
2023-01-06 09:38:25 +0000

    Linux 4f42aa569bc8 5.15.49-linuxkit #1 SMP Tue Sep 13 07:51:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:38:25 up 12 days, 19:28,  0 users,  load average: 0.08, 0.11, 0.05

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -144,8 +144,8 @@ int main()
     printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}%d \n", 1);
     test_1();
 
-    // printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
-    // test_2();
+    printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
+    test_2();
 
     printf("\n\n");
 
